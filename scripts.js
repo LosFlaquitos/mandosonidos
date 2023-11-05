816a3ff26a63955a8b8b992f9485023de27fb00e
ciclismo <63200014+LosFlaquitos@users.noreply.github.com>
2023-11-05 14:38:11 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,37 @@
+var currentAudio = null;
+
+function toggleAudio(audioId) {
+    var audio = document.getElementById(audioId);
+
+    if (currentAudio && currentAudio !== audio) {
+        currentAudio.pause(); // Pausar el audio actual si no es el mismo
+    }
+
+    if (audio.paused) {
+        audio.play(); // Iniciar la reproducción si está pausado
+        currentAudio = audio;
+    } else {
+        audio.pause(); // Pausar el audio si está reproduciéndose
+        currentAudio = null;
+    }
+}
+
+document.getElementById('boton1').addEventListener('click', function() {
+    toggleAudio('audio1');
+});
+
+document.getElementById('boton2').addEventListener('click', function() {
+    toggleAudio('audio2');
+});
+
+document.getElementById('boton3').addEventListener('click', function() {
+    toggleAudio('audio3');
+});
+
+document.getElementById('boton4').addEventListener('click', function() {
+    toggleAudio('audio4');
+});
+
+document.getElementById('boton5').addEventListener('click', function() {
+    toggleAudio('audio5');
+});
